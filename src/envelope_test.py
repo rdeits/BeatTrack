3e39350801b148c0f8c1acd75ab7310b58593c80
Robin Deits <robin.deits@gmail.com>
2012-02-03 16:50:51 -0500

    Stress test to compare the two envelope methods.
    "Fast" wins, espeically when width > 100

--- src/envelope_test.py
+++ src/envelope_test.py
@@ -1,11 +1,26 @@
 from listen import rolling_envelope, fast_rolling_envelope
 import numpy as np
+import timeit
 
-data = [np.random.random() for i in range(10000)]
+setup_stmt = """
+from listen import rolling_envelope, fast_rolling_envelope
+import numpy as np
+data = [np.random.random() for i in range(100000)]
+width = 100
+"""
 
-output = [rolling_envelope(data, i, 10) for i in range(len(data))]
+# data = [np.random.random() for i in range(10000)]
 
-output2 = fast_rolling_envelope(data, 10)
+t = timeit.Timer("""
+                 output = [rolling_envelope(data, i, width) for i in range(len(data))]
+                 """, setup_stmt) 
 
-assert all([abs(output[i] - output2[i]) < .0001 for i in range(len(output))]), [str(i) + "\n" + str(output[i]) + "\n" + str(output2[i]) for i in range(len(output)) if abs(output[i] - output2[i]) >= 1]
-print "passed test"
+t2 = timeit.Timer("""
+                  output2 = fast_rolling_envelope(data, width) #.556 seconds
+                  """, setup_stmt)
+
+print "Original:", t.timeit(10)/10.
+print "Fast:", t2.timeit(10)/10.
+
+# assert all([abs(output[i] - output2[i]) < .0001 for i in range(len(output))]), [str(i) + "\n" + str(output[i]) + "\n" + str(output2[i]) for i in range(len(output)) if abs(output[i] - output2[i]) >= 1]
+# print "passed test"
