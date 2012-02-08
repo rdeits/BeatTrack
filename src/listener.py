b4ddd6e415701b92c15c88c1fa5897cf3e06aabc
Robin Deits <robin.deits@gmail.com>
2012-02-08 01:51:57 -0500

    Tried some experiments with using convolution; better filtering.
    The useful difference here is that I'm now taking the derivative of the
    enveloped data and then enveloping it again. I also tried convolving
    instead of directly computing the comb, but it was slower

--- src/listener.py
+++ src/listener.py
@@ -2,7 +2,6 @@ from __future__ import division
 
 import wave
 import numpy as np
-import threading
 import time
 import pyaudio
 import matplotlib.pyplot as plt
@@ -36,15 +35,17 @@ def fast_rolling_envelope(data, width):
     return output
 
 class Listener(multiprocessing.Process):
-    def __init__(self, connection=None):
+    def __init__(self, live = True, connection=None, debug_connection=None):
         super(Listener, self).__init__()
         self.result = (0, 0, 0, 0)
         self.conn = connection
+        self.debug_conn = debug_connection
+        self.live = live
 
 
-    def open_stream(self, live = True):
+    def open_stream(self):
         self.block_size_s = 5
-        if live:
+        if self.live:
             ######################## PyAudio Block #############################
             p = pyaudio.PyAudio()
             self.stream = p.open(input_device_index = 3, 
@@ -84,7 +85,10 @@ class Listener(multiprocessing.Process):
                                               self.decimate_ratio, 
                                               n=3, 
                                               ftype="iir")
-        enveloped_data = fast_rolling_envelope(filtered_data, 10)
+        enveloped_data = fast_rolling_envelope(filtered_data, 50)
+        enveloped_data = np.diff(enveloped_data)
+        enveloped_data = np.max(np.vstack((enveloped_data, np.zeros_like(enveloped_data))), 0)
+        enveloped_data = fast_rolling_envelope(enveloped_data, 10)
         return enveloped_data
 
     def most_likely_bpm(self, enveloped_data, bpm_list):
@@ -109,22 +113,43 @@ class Listener(multiprocessing.Process):
         self.bpm_energies = np.array(all_energies)
         confidence = ((max(self.bpm_energies) - np.average(self.bpm_energies))
                       / np.std(self.bpm_energies))
+        # if not self.live:
+        #     plt.figure(1)
+        #     plt.plot(self.bpm_to_test, self.bpm_energies)
+        #     plt.figure(2)
+        #     plt.plot(enveloped_data)
+        #     plt.show()
+        # bpm_range = bpm_list[-1] - bpm_list[0]
+        # if bpm_range > 1:
+        #     return self.most_likely_bpm(enveloped_data, 
+        #                                 np.linspace(max_energy_bpm - bpm_range * .2,
+        #                                             max_energy_bpm + bpm_range * .2,
+        #                                             50))
+        # else:
         return (max_energy_bpm, max_energy, max_energy_phase, confidence)
-        # return (best_bpms, max_energy_phase, confidence)
 
     def trybeat(self, envelope, bpm):
         """Try a 3-sample comb filter on the envelope-filtered data at a given BPM. 
         Based on Ciuffo's implementation at 
         http://ch00ftech.com/2012/02/02/software-beat-tracking-because-a-tap-tempo-button-is-too-lazy/"""
+        gap = self.bpm_to_numsamples(bpm) #converts BPM to samples per beat
         num_teeth = self.calc_num_teeth(bpm)
         assert num_teeth > 3, "Block size too small"
-        gap = self.bpm_to_numsamples(bpm) #converts BPM to samples per beat
+        #####
+        # comb = np.zeros_like(envelope)
+        # comb[range(0, gap*num_teeth, gap)] = 1
+        # dft_envelope = np.fft.fft(envelope)
+        # dft_comb = np.fft.fft(comb)
+        # energy = np.sum(np.power(np.abs(dft_envelope * dft_comb), 2))/bpm**2
+        # phase = 0
+        ######
         comb_width = (num_teeth - 1) * gap
-        if (len(envelope)<=comb_width): #envelope waveform is too small to fit the comb.
+        if (len(envelope)<=comb_width): #envelope waveform is too small to fit comb
             return (0, 0)
         else:
             comb_positions = gap
-            comb_vals = np.array([np.sum(envelope[[-(i+j*gap) for j in range(num_teeth)]])
+            comb_vals = np.array([np.sum(envelope[[-(i+j*gap)
+                                                   for j in range(num_teeth)]])
                                   for i in range(comb_positions)])
             comb_vals = np.power(comb_vals, 4)
         energy = np.sum(comb_vals)/(comb_positions * num_teeth**4) # normalize
@@ -146,7 +171,8 @@ class Listener(multiprocessing.Process):
 
     def read_audio_block(self):
         elapsed_time = time.time() - self.read_timestamp
-        time.sleep(self.block_size_s - elapsed_time - 0.5)
+        if self.live:
+            time.sleep(self.block_size_s - elapsed_time - 0.5)
         available_samples = int(self.block_size_s * self.framerate)
         if self.sample_width == 1: 
             # read unsigned chars
@@ -163,7 +189,8 @@ class Listener(multiprocessing.Process):
 
     def run(self):
         self.open_stream()
-        self.bpm_to_test = range(90, 180)
+        # self.bpm_to_test = range(90, 180)
+        self.bpm_to_test = np.linspace(90, 180, 91)
         while True:
             data = self.read_audio_block()
             self.data_buffer = self.unpack_audio_data(data)
@@ -176,9 +203,11 @@ class Listener(multiprocessing.Process):
                 self.conn.send(self.result)
             else:
                 print "Most likely BPM:", bpm, "phase:", phase, "confidence:", confidence
+            if self.debug_conn is not None:
+                self.debug_conn.send((self.bpm_to_test, self.bpm_energies))
 
 if __name__ == "__main__":
-    listener = Listener()
+    listener = Listener(live = False)
     listener.start()
     listener.join()
             
